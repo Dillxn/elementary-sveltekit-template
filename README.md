2b0220c1ded61e4676db10ca9a0bc8aa8e980446
Dillon Riecke <dillon.riecke@gmail.com>
2023-03-21 18:02:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Introduction
+This is a basic SvelteKit server configured for spinning up Elementary.audio apps
+
+## Usage
+
+```
+npm i
+npm run dev
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { onMount } from 'svelte';
+	import { el } from '@elemaudio/core';
+	import { default as core } from '@elemaudio/plugin-renderer';
+
+
+	core.on('load', () => {
+
+		let left_in = el.in({ channel: 0 });
+		let right_in = el.in({ channel: 1 });
+
+		core.render(left_in, right_in);
+	});
+
+	onMount(() => {
+		core.initialize();
+	});
+</script>
+
+<main>
+	<h1>Hello world!</h1>
+</main>
+	
+<style>
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
